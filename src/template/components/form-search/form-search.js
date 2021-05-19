7e8872216f02ad33ccd88a2809d205188eb4889b
Piotr-Setsko <lihadzey@gmail.com>
2021-05-19 21:41:20 +0300

    feat: add style from text and submit input

--- src/template/components/form-search/form-search.js
+++ src/template/components/form-search/form-search.js
@@ -1,7 +1,8 @@
-var dropdown = document.querySelectorAll('.dropdown');
+const dropdownTrigger = document.querySelectorAll('.form-search__dropdown-trigger');
+const dropdown = document.querySelectorAll('.dropdown');
 
-for (let i = 0; i < dropdown.length; i++) {
- dropdown[i].addEventListener("click", () => {
+for (let i = 0; i < dropdownTrigger.length; i++) {
+ dropdownTrigger[i].addEventListener("click", () => {
     dropdown[i].classList.toggle("is-active");
   });
 }
