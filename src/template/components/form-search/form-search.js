155356a5e6103624f26821afd6d48c4e377790ac
Piotr-Setsko <lihadzey@gmail.com>
2021-05-21 08:29:57 +0300

    feat: add menu visibility

--- src/template/components/form-search/form-search.js
+++ src/template/components/form-search/form-search.js
@@ -1,5 +1,7 @@
 const dropdownTrigger = document.querySelectorAll('.form-search__dropdown-trigger');
 const dropdown = document.querySelectorAll('.dropdown');
+const mql = window.matchMedia("(max-width: 760px)")
+
 
 for (let i = 0; i < dropdownTrigger.length; i++) {
  dropdownTrigger[i].addEventListener("click", () => {
@@ -7,3 +9,14 @@ for (let i = 0; i < dropdownTrigger.length; i++) {
   });
 }
 
+mql.addEventListener('change', (e) => {
+  if (e.matches) {
+    for (let i = 0; i < dropdownTrigger.length; i++) {
+      dropdown[i].classList.remove("is-active");
+    }
+  } else {
+    for (let i = 0; i < dropdownTrigger.length; i++) {
+      dropdown[i].classList.add("is-active");
+    }
+  }
+})
